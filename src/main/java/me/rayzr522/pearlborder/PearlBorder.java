a8f77328c696b163c9b4d115311f215e574635a3
Rayzr522 <rayzr522@gmail.com>
2019-08-21 19:58:57 -0400

    Added in threshold and fixed logic

--- src/main/java/me/rayzr522/pearlborder/PearlBorder.java
+++ src/main/java/me/rayzr522/pearlborder/PearlBorder.java
@@ -3,6 +3,7 @@ package me.rayzr522.pearlborder;
 import org.bukkit.World;
 import org.bukkit.WorldBorder;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,12 +12,18 @@ import org.bukkit.plugin.java.JavaPlugin;
  * @author Rayzr
  */
 public class PearlBorder extends JavaPlugin implements Listener {
+    private double threshold;
+
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(this, this);
+
+        saveDefaultConfig();
+
+        threshold = getConfig().getDouble("threshold", 2.0);
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.HIGH, ignoreCancelled = true)
     public void onTeleport(PlayerTeleportEvent e) {
         if (e.getCause() != PlayerTeleportEvent.TeleportCause.ENDER_PEARL) {
             return;
@@ -32,9 +39,9 @@ public class PearlBorder extends JavaPlugin implements Listener {
 
         double offsetX = e.getTo().getX() - worldBorder.getCenter().getX();
         double offsetZ = e.getTo().getZ() - worldBorder.getCenter().getZ();
-        double size = worldBorder.getSize() / 2.0;
+        double size = worldBorder.getSize() / 2.0 - threshold;
 
-        if (offsetX > size || offsetX < -size || offsetZ > size || offsetZ < -size) {
+        if (Math.abs(offsetX) > size || Math.abs(offsetZ) > size) {
             e.setCancelled(true);
         }
     }
